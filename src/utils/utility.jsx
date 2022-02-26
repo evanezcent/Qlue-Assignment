622ded341c61fda7d1af1e58c21d85d5c10505e9
evanezcent <pratamayo16@gmail.com>
2022-02-27 00:36:18 +0700

    fixing warning

--- src/utils/utility.jsx
+++ src/utils/utility.jsx
@@ -4,26 +4,23 @@ const parseObject = (data) => {
   let res = [];
 
   if (Object.keys(data).length > 0) {
-    Object.keys(data).map((key) => {
-      if (isExpert(data[key])) {
-        res.push(key.toString());
-      }
-    });
+    Object.keys(data).map(
+      (key) => isExpert(data[key]) || res.push(key.toString())
+    );
   }
 
   return res;
 };
 
 const restructureData = (data) => {
-  let tempData = [];
-  data.map((item, idx) => {
+  let tempData = data.map((item) => {
     let tempSkill = parseObject(item.skills);
 
-    tempData.push({
+    return {
       id: item.id,
       full_name: `${item.first_name} ${item.last_name}`,
       expert_skills: tempSkill,
-    });
+    };
   });
 
   return tempData;
